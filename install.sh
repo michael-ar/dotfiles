04e1e942f2958cf9c9fec8de061b40c9162fd7e9
Michael Reynolds <michaelandreynolds@gmail.com>
2022-01-07 15:56:54 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &&
+/usr/bin/bash -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install)" &&
 brew tap caskroom/cask &&
 brew install cask
 
